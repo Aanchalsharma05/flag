400774e72c144577542d1e70b832bbd6adeb994f
Aanchalsharma05 <131364628+Aanchalsharma05@users.noreply.github.com>
2023-06-21 18:32:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+function indiaFunc() {
+    var c = "This is Flag of India";
+    document.getElementById("india").innerHTML = c;
+}
+
+function outindia() {
+    var c = "";
+    document.getElementById("india").innerHTML = c;
+
+}
+//flag 2
+
+function brazilFunc() {
+    var c = "This is Flag of Brazil";
+    document.getElementById("brazil").innerHTML = c;
+}
+
+function outbrazil() {
+    var c = "";
+    document.getElementById("brazil").innerHTML = c;
+
+}
+
+//flag 3
+function argentinaFunc() {
+    var c = "This is Flag of argentina";
+    document.getElementById("argentina").innerHTML = c;
+}
+
+function outargentina() {
+    var c = "";
+    document.getElementById("argentina").innerHTML = c;
+
+}
+//flag 4
+function australiaFunc() {
+    var c = "This is Flag of australia";
+    document.getElementById("australia").innerHTML = c;
+}
+
+function outaustralia() {
+    var c = "";
+    document.getElementById("australia").innerHTML = c;
+
+}
+//flag 5
+function portugalFunc() {
+    var c = "This is Flag of portugal";
+    document.getElementById("portugal").innerHTML = c;
+}
+
+function outportugal() {
+    var c = "";
+    document.getElementById("portugal").innerHTML = c;
+
+}
+
+//flag 6
+function newzealandFunc() {
+    var c = "This is Flag of Newzealand";
+    document.getElementById("newzealand").innerHTML = c;
+}
+function outnewzealand() {
+    var c = "";
+    document.getElementById("newzealand").innerHTML = c;
+
+}
+
+//flag 7
+function outcanada() {
+    var c = "";
+    document.getElementById("canada").innerHTML = c;
+
+}
+
+function canadaFunc() {
+    var c = "This is Flag of canada";
+    document.getElementById("canada").innerHTML = c;
+}
+
+//flag8 
+function outdubai() {
+    var c = "";
+    document.getElementById("dubai").innerHTML = c;
+
+}
+function dubaiFunc() {
+    var c = "This is Flag of dubai";
+    document.getElementById("dubai").innerHTML = c;
+}
+
+//flag 9
+function outengland() {
+    var c = "";
+    document.getElementById("england").innerHTML = c;
+
+}
+function englandFunc() {
+    var c = "This is Flag of england";
+    document.getElementById("england").innerHTML = c;
+}
+
+//flag 10
+function outrussia() {
+    var c = "";
+    document.getElementById("russia").innerHTML = c;
+
+}
+function russiaFunc() {
+    var c = "This is Flag of russia";
+    document.getElementById("russia").innerHTML = c;
+}
+
+
+
